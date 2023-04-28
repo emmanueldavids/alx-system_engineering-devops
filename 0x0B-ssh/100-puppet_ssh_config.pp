d16ca1c2c78ca33fa2de15c2e97b422372524e01
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-28 14:41:47 +0000

    Files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# this is a puppet script to create and modify the ssh config file in the etc directory
+file { '/etc/ssh/sshd_config':
+  ensure  => present,
+  mode    => '0600',
+  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+}
